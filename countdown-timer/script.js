89b12a4d80cb9499414561db21f1306344f23db5
hicoderindia <hicoder.dolly@gmail.com>
2022-02-13 19:02:53 +0530

    timer completed

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -1,24 +1,30 @@
-const valentineDay= "14 feb 2022";
+const valentineDay = "14 feb 2022";
+
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
 
 function countdown() {
-    const valentinedayDate= new Date(valentineDay);
-    const currentDate= new Date();
+  const valentinedayDate = new Date(valentineDay);
+  const currentDate = new Date();
 
-    const totalSecond= (valentinedayDate-currentDate)/1000;
-    const days= Math.floor(totalSecond/3600/24);
-    const hours= Math.floor(totalSecond/3600)% 24;
-    const minutes= Math.floor(totalSecond/60)% 60;
-    const seconds= Math.floor(totalSecond)%60;
+  const totalSecond = (valentinedayDate - currentDate) / 1000;
+  const days = Math.floor(totalSecond / 3600 / 24);
+  const hours = Math.floor(totalSecond / 3600) % 24;
+  const mins = Math.floor(totalSecond / 60) % 60;
+  const seconds = Math.floor(totalSecond) % 60;
 
-    console.log(days, hours, minutes, seconds);
+  console.log(days, hours, mins, seconds);
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = hours;
+  minsEl.innerHTML = mins;
+  secondsEl.innerHTML = seconds;
 
-    // console.log(valentinedayDate-currentDate);
+  // console.log(valentinedayDate-currentDate);
 }
-
 
 //calling the function
 countdown();
 
-
-setInterval(countdown, 1000)
-
+setInterval(countdown, 1000);
